588c5b0f85f655bde666dd80dee82318650d9d4e
Bharadwaj <115616330+bharadwaj-phoneix@users.noreply.github.com>
2024-06-06 10:10:00 +0530

    Spiral Matrix

---
+++ 54.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        if (matrix.empty() || matrix[0].empty()) {
+            return res;
+        }
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int top = 0, bottom = m - 1, left = 0, right = n - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int j = left; j <= right; j++) {
+                res.push_back(matrix[top][j]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                res.push_back(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    res.push_back(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    res.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return res;
+    }
+};
