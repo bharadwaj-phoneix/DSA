0523f5f21705ae6716857ce9f88adb4b9aebc30c
Bharadwaj <115616330+bharadwaj-phoneix@users.noreply.github.com>
2024-06-11 12:28:42 +0530

    Simplify Path

---
+++ 71.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack <string> s;
+        string res;
+        for(int i=0;i<path.length();i++){
+            if(path[i]=='/'){
+                continue;
+            }
+            string temp;
+            while(i<path.size() && path[i]!='/'){
+                temp += path[i];
+                i++;
+            }
+            if(temp == "."){
+                continue;
+            }
+            else if(temp==".."){
+                if(!s.empty()){s.pop();}
+            }
+            else{
+                s.push(temp);
+            }
+
+        }
+        while(!s.empty()){
+            res = "/" + s.top() + res;
+            s.pop();
+        }
+        if(res.size()==0){
+            return "/";
+        }
+        return res;
+    }
+};
